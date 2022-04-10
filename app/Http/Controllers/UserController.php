11deadd4cfbee34efeff856951054c6478d87f3b
alfin ardian <developer.alfin@gmail.com>
2022-04-10 07:02:44 +0700

    menambah template admin argon

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Models\User;
+use App\Http\Requests\UserRequest;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function list(Request $request) {
-        return User::get();
+    /**
+     * Display a listing of the users
+     *
+     * @param  \App\Models\User  $model
+     * @return \Illuminate\View\View
+     */
+    public function index(User $model)
+    {
+        return view('users.index');
     }
 }
